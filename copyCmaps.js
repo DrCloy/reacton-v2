40db3484bd8b9235ba5f7511f6c0ac13bdb3d5f7
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-25 15:56:02 +0900

    publish 2025-02-25T15:56:02

--- copyCmaps.js
+++ copyCmaps.js
@@ -1,6 +1,9 @@
-// copyCmaps.js
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function copyFolderSync(from, to) {
   if (!fs.existsSync(to)) {
